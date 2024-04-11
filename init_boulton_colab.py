2c445e7dcf6fdf9fedb9c79e2bf5c252a2128258
Professor Philip Lewis <ucfalew@ucl.ac.uk>
2024-04-11 17:25:05 +0100

    Create init_boulton_colab.py

---
+++ init_boulton_colab.py
@@ -0,0 +1,33 @@
+# for colab
+import os
+from getpass import getpass
+import urllib
+from pathlib import Path
+import shutil
+
+refresh = True
+
+here = Path.cwd().parts
+print(f'Currently in directory {Path.cwd()}')
+# this will tell us we are in colab
+condition = refresh and here[-1] == 'boulton' and here[-2] == 'content'
+if condition:
+    os.chdir('..')
+    print(Path.cwd())
+    shutil.rmtree('boulton')
+else:
+  print(condition,'regeneration switch:',refresh,'in',here[-1],here[-2])
+# test if in correct directory
+if not Path.cwd().parts[-1] == 'boulton':
+  if not Path('boulton/loaded.txt').exists():
+    try:
+      from google.colab import userdata
+      password = userdata.get('pat_boulton:')
+    except:
+      password = getpass('Password: ')
+      password = urllib.parse.quote(password) 
+    !git clone https://'{password}:{password}'@github.com/profLewis/boulton.git
+  
+  os.chdir('boulton')  
+  !pwd
+
