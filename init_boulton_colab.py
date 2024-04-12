4dfbb9dc97d6887bcac4243e3b9bc176016ad1eb
Professor Philip Lewis <ucfalew@ucl.ac.uk>
2024-04-12 11:49:40 +0100

    Update init_boulton_colab.py

--- init_boulton_colab.py
+++ init_boulton_colab.py
@@ -1,39 +1,34 @@
 # for colab
 import os
-from getpass import getpass
-import urllib
 from pathlib import Path
-import shutil
 
-refresh = True
-
-here = Path.cwd().parts
-print(f'Currently in directory {Path.cwd()}')
-# this will tell us we are in colab
-condition = refresh and here[-1] == 'boulton' and here[-2] == 'content'
-if condition:
-    os.chdir('..')
-    print(Path.cwd())
-    shutil.rmtree('boulton')
-else refresh here[-2] == 'content':
+try:
+  # ie this is colab
+  os.chdir('/content')
+  print("I think this is colab ...")
+  if refresh:
+    print(f"refresh set {refresh} so cleaning up ...")
     try:
-      print(Path.cwd())
+      import shutil
       shutil.rmtree('boulton')
     except:
       pass
+  else:
+    print(f"refresh set {refresh}");
 
-  print(condition,'regeneration switch:',refresh,'in',here[-1],here[-2])
-# test if in correct directory
-if not Path.cwd().parts[-1] == 'boulton':
-  if not Path('boulton/loaded.txt').exists():
+  from pathlib import Path
+  if not Path('boulton').exists():
+    # try to get the required token (used as a passwd)
     try:
+      # in case token is stored as secret (pat_boulton)
       from google.colab import userdata
       password = userdata.get('pat_boulton:')
     except:
+      from getpass import getpass
+      import urllib
       password = getpass('Password: ')
-      password = urllib.parse.quote(password) 
+      password = urllib.parse.quote(password)
     os.system(f"git clone https://{password}:{password}@github.com/profLewis/boulton.git")
-  
-  os.chdir('boulton')  
-  os.system("pwd")
+except:
+  pass
 
